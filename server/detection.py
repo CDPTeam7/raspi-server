b92c7a06918e2cb6fd2b5b8e7ff828304d72d796
lnsqj123 <holmane33@gmail.com>
2024-06-15 17:35:47 +0900

    [back] hotfix: 추론결과 시각화

--- server/detection.py
+++ server/detection.py
@@ -1,13 +1,12 @@
-# from deepface import DeepFace
+from deepface import DeepFace
 from database import db
-
-# import math
+import math
 import cv2
 from ultralytics import YOLO
+import torch
+import os
 
-# import torch
-
-pet_model = YOLO("./server/model/best.pt")
+pet_model = YOLO("./server/model/best232.pt")
 
 classNames = [
     "0",
@@ -38,13 +37,13 @@ classNames = [
 ]
 
 
-def image_detection(image_path):
+def image_detection(image_path, save_path):
     # --------------------------------추론----------------------------------#
     # 이미지 불러오기
     img = cv2.imread(image_path)
     img = cv2.resize(img, (640, 640))
-    """
     # 추론 실행
+    """
     if torch.cuda.is_available():  # CUDA 지원하는 gpu(Nvidia gpu)있는 경우 gpu 사용
         results = pet_model.predict(img, device="0")
     else:
@@ -59,14 +58,50 @@ def image_detection(image_path):
     for r in results:
         boxes = r.boxes
         for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = math.ceil((box.conf[0] * 100)) / 100
             cls = int(box.cls[0])
             class_name = classNames[cls]
+            label = f"{class_name}_{conf}"
+            t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
             if class_name in ["2", "4", "6", "8", "10", "12"]:  # target plastic bottle
+                # if class_name == "0":  # target plastic bottle
                 target_count += 1
+                color = (222, 82, 175)
+            # elif class_name == "4": # other plastic bottle
+            #     color = (0, 149, 255)
+            else:  # not plastic bottle
+                color = (85, 45, 255)
+            # if conf > 0.25:
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+            cv2.rectangle(img, (x1, y1), c2, color, -1, cv2.LINE_AA)
+            # Adjust text location if it goes out of frame
+            if y1 - t_size[1] < 0:
+                text_y = y1 + t_size[1] + 3
+            else:
+                text_y = y1 - 2
+
+            cv2.putText(
+                img,
+                label,
+                (x1, text_y),
+                0,
+                1,
+                [255, 255, 255],
+                thickness=1,
+                lineType=cv2.LINE_AA,
+            )
+
+    print(f"target count : {target_count}")
+    result_image_path = os.path.join(save_path, os.path.basename(image_path))
+
+    # 추론 결과 저장
+    cv2.imwrite(result_image_path, img)
+
     return target_count
 
 
-"""
 # target image의 embedding 정보 얻기
 def get_target_embedding(target_img_path):
     try:
@@ -117,15 +152,15 @@ def verify(target, candidate):
     result = DeepFace.verify(
         img1_path=target,
         img2_path=candidate,
-        model_name="ArcFace",
-        detector_backend="opencv",
+        model_name="Facenet512",
+        detector_backend="retinaface",
         distance_metric="cosine",
-        enforce_detection=True,
+        enforce_detection=False,
         align=True,
         expand_percentage=0,
         normalization="base",
         silent=False,
+        # threshold = 0.5
     )
 
     return result["verified"]
-"""
